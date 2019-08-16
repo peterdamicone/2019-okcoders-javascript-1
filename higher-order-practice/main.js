d6f4e1c0f98380f2362139bce554674be0d0ab19
peterdamicone <peter.damicone@gmail.com>
2019-08-16 12:58:39 -0500

    more practice with filter using free code camp

--- higher-order-practice/main.js
+++ higher-order-practice/main.js
@@ -1,27 +1,6 @@
-const companies = [
-    { name: "Company One", category: "Finance", start: 1981, end: 2004 },
-    { name: "Company Two", category: "Retail", start: 1992, end: 2008 },
-    { name: "Company Three", category: "Auto", start: 1999, end: 2007 },
-    { name: "Company Four", category: "Retail", start: 1989, end: 2010 },
-    { name: "Company Five", category: "Technology", start: 2009, end: 2014 },
-    { name: "Company Six", category: "Finance", start: 1987, end: 2010 },
-    { name: "Company Seven", category: "Auto", start: 1986, end: 1996 },
-    { name: "Company Eight", category: "Technology", start: 2011, end: 2016 },
-    { name: "Company Nine", category: "Retail", start: 1981, end: 1989 }
-];
+const people = [{ name: "John Doe", age: 16 }, { name: "Thomas Calls", age: 19 }, { name: "Liam Smith", age: 20 }, { name: "Jessy Pinkman", age: 18 }, ];
+// exersizes from https://www.freecodecamp.org/news/higher-order-functions-in-javascript-d9101f9cf528/
+// Imagine writing a piece of code that accepts a list of people where you want to filter out the people that are equal or above the age of 18.
 
-const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
-
-// CH5E1 Flattening
-// Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has all the elements of the original arrays.
-let arrays = [
-    [1, 2, 3],
-    [4, 5],
-    [6]
-];
-
-const flaten = arrays.reduce(0, 0, 0, arrays);
-console.log(flaten);
-const flaten = arrays.reduce(arrays.concat(arrays[0], arrays[1], arrays[2]));
-console.log(arrays.reduce((flat, current) => flat.concat(current), []));
-console.log(arrays.reduce((flat, current) => flat.concat(current), []));
+const under18 = people.filter(age => age.age <= 18);
+console.log(under18);
